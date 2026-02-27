1dd0b2d053b569e58ddf985192aaab08c5dd99b3
Andres Sneider Garcia Quintero <sneider7896@gmail.com>
2026-02-26 20:45:46 -0500

    Delete env prod and change enviroment

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: 'https://nest-teslo-shop-04h7.onrender.com/api'
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  baseUrl: 'https://nest-teslo-shop-04h7.onrender.com/api'
+  production: true,
+  baseUrl: 'https://nest-teslo-shop-04h7.onrender.com/api',
 };
